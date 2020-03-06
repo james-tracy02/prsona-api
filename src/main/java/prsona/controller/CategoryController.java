cbc8a748369ce46648ef86579f7a3288316d9a49
James Tracy <jtracy.emmet@gmail.com>
2020-03-06 15:45:25 -0500

    get/post test

--- src/main/java/prsona/controller/CategoryController.java
+++ src/main/java/prsona/controller/CategoryController.java
@@ -6,6 +6,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import prsona.model.Category;
@@ -21,5 +23,10 @@ public class CategoryController {
 	@GetMapping("/categories/{id}")
 	public Optional<Category> findCategoryById(@PathVariable int id) {
 		return categoryRepository.findById(id);
+	}
+	
+	@PostMapping("/categories")
+	public void createCategory(@RequestBody Category category) {
+		categoryRepository.save(category);
 	}
 }
