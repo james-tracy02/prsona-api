9ad57220cbfb30c22d6b04c87f59cbf549324066
James Tracy <jtracy.emmet@gmail.com>
2020-03-18 02:08:50 -0400

    users/auth

--- src/main/java/prsona/controller/UserController.java
+++ src/main/java/prsona/controller/UserController.java
@@ -4,7 +4,6 @@ import java.util.Base64;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -20,12 +19,8 @@ public class UserController {
 	@Autowired
 	UserRepository userRepository;
 	
-	@Autowired
-	private PasswordEncoder passwordEncoder;
-	
 	@PostMapping("/users")
 	public void createUser(@RequestBody User user) {
-		user.setPassword(passwordEncoder.encode(user.getPassword()));
 		userRepository.save(user);
 	}
 	
@@ -39,7 +34,7 @@ public class UserController {
 		String decodedToken = new String(decodedBytes);
 		String[] splitToken = decodedToken.split(":");
 		return target.getUsername() == splitToken[0] 
-				&& passwordEncoder.matches(splitToken[1], target.getPassword());
+				&& target.getPassword() == splitToken[1];
 		
 	}
 }
